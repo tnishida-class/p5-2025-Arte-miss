e4b19425673c496f6f4e14d0dd4c62c51e5e6236
Arte-miss <sirochankurochan22@docomo.ne.jp>
2025-11-23 19:12:23 +0900

    sonota

--- Q3-2/sketch.js
+++ Q3-2/sketch.js
@@ -2,20 +2,21 @@
 let x, y;
 let vx, vy;
 const g = 1;
+const vyMax = 20;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 0;
-  vy = 0;
+  vx = 2;
+  vy = 2;
 }
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
+function draw() {
   background(160, 192, 255);
   const size = height * 0.1; // キャラクターのサイズ
 
@@ -24,19 +25,41 @@ function draw(){
   fill(64, 192, 64);
   rect(0, groundY, width, height - groundY);
 
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
+    if (keyIsDown(SHIFT)) { x -= 5; }
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
+    if (keyIsDown(SHIFT)) { x += 5; }
+  }
+  if (keyIsDown(UP_ARROW)) { y -= 5; }
+  if (keyIsDown(DOWN_ARROW)) { y += 5; }
   // BLANK[1] キャラクターの左右移動
 
-  // BLANK[2] 重力とジャンプ
+  y += vy
+  vy += g;
+  y = constrain(y, 0, height * 0.8 - size / 2);
+
+  // BLANK[2] 重力とジャンプ 
+
+
 
   // 速くなりすぎないように制限
   vx = constrain(vx, -20, 20);
   vy = constrain(vy, -20, 20);
 
   // 位置を更新
-  x += vx;
-  y += vy;
 
   // キャラクターを描く
   fill(0);
   ellipse(x, y, size, size);
 }
+function keyPressed() {
+    if (key == " " && y== constrain(y, height * 0.75, height * 0.8)) {
+      vy = -vy * 1;
+    }
+  }
+
+
+
